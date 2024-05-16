ea7ed17773d47c2033c3f9d5199f24f4ad081f15
EC2 Default User <ec2-user@ip-172-31-61-50.ec2.internal>
2024-05-16 00:17:48 +0000

    first commit

--- my_pipeline/my_pipeline_stack.py
+++ my_pipeline/my_pipeline_stack.py
@@ -10,10 +10,12 @@ class MyPipelineStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "MyPipelineQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        pipeline =  CodePipeline(self, "Pipeline",
+                        pipeline_name="MyPipeline",
+                        synth=ShellStep("Synth",
+                            input=CodePipelineSource.git_hub("OWNER/REPO", "main"),
+                            commands=["npm install -g aws-cdk",
+                                "python -m pip install -r requirements.txt",
+                                "cdk synth"]
+                        )
+                    )
